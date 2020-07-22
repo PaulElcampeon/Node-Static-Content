b5a10b76b7b492fa8f048af0db67bc198bc8e8c1
PaulElcampeon <pauloladele@live.co.uk>
2020-07-22 19:53:54 +0100

    Added controller to router.

--- src/router.js
+++ src/router.js
@@ -1,5 +1,9 @@
 const express = require("express")
 
+const controller = require('./controllers/controller')
+
 const router = express.Router();
+
+router.get('*', controller.renderMarkdown);
 
 module.exports = router;
