9e0e12cd9f7b1dcaa671b50a00e11c909e02888c
PaulElcampeon <pauloladele@live.co.uk>
2020-07-22 19:54:51 +0100

    Using router in server.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require("express")
 
+const router = require("./router")
+
 const app = express()
 
 app.set("views", "views")
 
 app.set("view engine", "hbs")
+
+app.use("/", router)
 
 app.listen(3000, () => {
     console.log("The server is now running on port 3000!")
